0e81d275580aa3a26db6a64ca9a73d3dfedb68af
jasmine <brackpinkfriday@gmail.com>
2024-10-05 21:57:59 +0900

    タスク登録ページ”

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_mail import Mail, Message
 from apscheduler.schedulers.background import BackgroundScheduler
 from config import Config
-from models import db
-from routes import main as main_routes
+from models import db, Task
+import datetime
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -31,11 +31,25 @@ scheduler.start()
 with app.app_context():
     db.create_all()
 
-app.register_blueprint(main_routes)
-
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    data = request.get_json()
+    due_date = datetime.datetime.strptime(data['due_date'], '%Y-%m-%dT%H:%M')
+
+    new_task = Task(
+        title=data['title'],
+        due_date=due_date,
+        requester_email=data['requester_email'],
+        assignee_email=data['assignee_email']
+    )
+    db.session.add(new_task)
+    db.session.commit()
+    
+    return jsonify({'message': 'Task added'}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -8,9 +8,3 @@ class Task(db.Model):
     due_date = db.Column(db.DateTime, nullable=False)
     requester_email = db.Column(db.String(100), nullable=False)
     assignee_email = db.Column(db.String(100), nullable=False)
-    comments = db.relationship('Comment', backref='task', lazy=True)
-
-class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    task_id = db.Column(db.Integer, db.ForeignKey('task.id'), nullable=False)
-    content = db.Column(db.String(200), nullable=False)
