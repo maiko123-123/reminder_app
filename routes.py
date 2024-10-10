5a0d7823cd8089d536a20778c35aec4eb2d827c9
jasmine <brackpinkfriday@gmail.com>
2024-10-10 21:03:05 +0900

    依頼者追加

--- routes.py
+++ routes.py
@@ -18,7 +18,7 @@ def task_list():
 def task_detail(task_id):
     task = Task.query.get(task_id)
     comments = Comment.query.filter_by(task_id=task_id).all()
-    return render_template('task_detail.html', task=task, comments=comments)
+    return render_template('task_detail.html', task=task, comments=comments, requester=task.requester)
 
 @main.route('/add_comment/<int:task_id>', methods=['POST'])
 def add_comment(task_id):
